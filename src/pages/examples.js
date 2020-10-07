670a3529e2e5b2f2ecc5c47e7c9abb2ccd0a51c8
sz16900 <sz16900@my.bristol.ac.uk>
2020-10-07 10:06:28 -1000

    page query

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,14 +1,39 @@
 import React from "react"
 import Header from "../components/examples/Header"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-const examples = () => {
+const examples = ({ data }) => {
+  const {
+    site: {
+      siteMetadata: { author },
+    },
+  } = data
+
   return (
     <Layout>
       <h1>Hello from examples page</h1>
       <Header />
+      <h5>author: {author}</h5>
     </Layout>
   )
 }
+
+export const data = graphql`
+  query {
+    site {
+      siteMetadata {
+        author
+        data
+        description
+        title
+        person {
+          age
+          name
+        }
+      }
+    }
+  }
+`
 
 export default examples
